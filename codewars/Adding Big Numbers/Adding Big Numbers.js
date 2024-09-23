797dae4cb932bca323d05df329fe5d84dd18131f
mutil2 <146696499+mutil2@users.noreply.github.com>
2024-09-23 08:50:12 +0300

    Adding Big Numbers.js

---
+++ codewars/Adding Big Numbers/Adding Big Numbers.js
@@ -0,0 +1,16 @@
+function add(num1, num2) {
+    let result = '';
+    let carry = 0;
+    while (num1.length < num2.length) num1 = '0' + num1;
+    while (num2.length < num1.length) num2 = '0' + num2;
+    for (let i = num1.length - 1; i >= 0; i--) {
+        let sum = parseInt(num1[i]) + parseInt(num2[i]) + carry;
+        let digit = sum % 10;
+        carry = Math.floor(sum / 10);
+        result = digit + result;
+    }
+    if (carry > 0) {
+        result = carry + result;
+    }
+    return result;
+}
