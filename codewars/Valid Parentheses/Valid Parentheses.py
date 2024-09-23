85eb1dde85e50157ede8845ffe21be63ea699e8f
mutil2 <146696499+mutil2@users.noreply.github.com>
2024-09-23 08:25:25 +0300

    Valid Parentheses.py

---
+++ codewars/Valid Parentheses/Valid Parentheses.py
@@ -0,0 +1,10 @@
+def valid_parentheses(s):
+    stack = []
+    for char in s:
+        if char == '(':
+            stack.append(char)
+        elif char == ')':
+            if not stack:
+                return False
+            stack.pop()
+    return not stack
