79629f3def80840efe22b9a03588666734c30f38
gureviv <gurevivaniv>
2024-10-04 13:39:19 +0300

    test4lab

--- rpgsaga/saga/tests/example.spec.ts
+++ rpgsaga/saga/tests/example.spec.ts
@@ -1,5 +1,32 @@
-import { Znachenie_Funkcii,TaskA,TaskB } from "../src";   
+import { Znachenie_Funkcii,TaskA,TaskB } from "../src";
+
 describe('Znachenie_Funkcii', () => {
-    // dodelayu
+  it('should return the correct result for x<1', () => {
+    expect(Znachenie_Funkcii(0.2)).toBeCloseTo(1.369);
   });
+  it('should return the correct result for x<1', () => {
+    expect(Znachenie_Funkcii(0)).toBeCloseTo(1.57);
+  });
+  it('should return the correct result for x<1', () => {
+    expect(Znachenie_Funkcii(-2)).toBeCloseTo(-0.857);
+  });
+  it('should return the correct result for x<1', () => {
+    expect(Znachenie_Funkcii(10)).toBeCloseTo(-9.657);
+  });
+});
+
+
+describe('TaskA', () => {
+    it('should return the correct lenght of result', () => {
+      const [x, y] = TaskA(0.2, 2.2, 0.4);
+      expect(y).toHaveLength(6);
+    });
+});
+
+describe('TaskB', () => {
+    it('should return the correct result', () => {
+      const [x, y] = TaskB([0.1, 0.9, 1.2, 1.5, 2.3]);
+      expect(y).toHaveLength(5);
+    });
+});
   
