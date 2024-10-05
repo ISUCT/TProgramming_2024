3d16fc0e5123e35304c2925c949329d431814d61
__HoLeN__ <holenyt@gmail.com>
2024-10-05 13:39:17 +0300

    fix lint

--- rpgsaga/saga/src/Employee.ts
+++ rpgsaga/saga/src/Employee.ts
@@ -1,38 +1,38 @@
-export default class Employee {
-    private _name: string;
-    private _age: number;
-    private _salary: number;
-    
-    constructor(_name: string, _age: number, _salary: number = 15000) {
-        this.name = _name;
-        this.age = _age;
-        this.salary = _salary;
-    }
+export class Employee {
+  private nameC: string;
+  private ageC: number;
+  private salaryC: number;
 
-    public get name(): string {
-        return this._name;
+  constructor(name: string, age: number, salary: number = 15000) {
+    this.name = name;
+    this.age = age;
+    this.salary = salary;
+  }
+
+  public get name(): string {
+    return this.nameC;
+  }
+  public set name(name: string) {
+    this.nameC = name;
+  }
+  public get age(): number {
+    return this.ageC;
+  }
+  public set age(age: number) {
+    if (age >= 0) {
+      this.ageC = age;
+      return;
     }
-    public set name(_name: string) {
-        this._name = _name;
+    throw new Error('Возраст не может быть отрицательным.');
+  }
+  public get salary(): number {
+    return this.salaryC;
+  }
+  public set salary(salary: number) {
+    if (salary >= 0) {
+      this.salaryC = salary;
+      return;
     }
-    public get age(): number {
-        return this._age;
-    }
-    public set age(_age: number) {
-        if (_age >= 0) {
-            this._age = _age;
-            return;
-        }
-        throw new Error('Возраст не может быть отрицательным.')
-    }
-    public get salary(): number {
-        return this._salary;
-    }
-    public set salary(_salary: number) {
-        if (_salary >= 0) {
-            this._salary = _salary;
-            return;
-        }
-        throw new Error('Заплата не может быть отрицательной.')
-    }
+    throw new Error('Заплата не может быть отрицательной.');
+  }
 }
--- rpgsaga/saga/src/index.ts
+++ rpgsaga/saga/src/index.ts
@@ -1,46 +1,43 @@
-import Employee from "./Employee";
+import { Employee } from './Employee';
 
-const func = (x: number, a: number = 4.1, b: number = 2.7): number => {
-    return ((b * Math.sqrt(x)) - (a * log(x, 5))) / (log(Math.abs(x - 1), 10));
-}
+const func = (x: number, a: number = 4.1, b: number = 2.7): number =>
+  (b * Math.sqrt(x) - a * log(x, 5)) / log(Math.abs(x - 1), 10);
 
-const log = (x: number, j: number): number => {
-    return Math.log(x) / Math.log(j);
-}
+const log = (x: number, j: number): number => Math.log(x) / Math.log(j);
 
-const task_a = (x_start: number, x_end: number, x_delta: number): number[] => {
-    let a_result: number[] = [];
-    for (let i: number = x_start; i<=x_end; i+=x_delta) {
-        a_result.push(func(i));
-    }
-    return a_result;
-}
+const taskA = (xStart: number, xEnd: number, xDelta: number): number[] => {
+  const aResult: number[] = [];
+  for (let i: number = xStart; i <= xEnd; i += xDelta) {
+    aResult.push(func(i));
+  }
+  return aResult;
+};
 
-const task_b = (xs: number[]): number[] => {
-    let b_result: number[] = [];
-    for (let i of xs) {
-        b_result.push(func(i));
-    }
-    return b_result;
-}
+const taskB = (xs: number[]): number[] => {
+  const bResult: number[] = [];
+  for (const i of xs) {
+    bResult.push(func(i));
+  }
+  return bResult;
+};
 
-export {func, log, task_a, task_b};
+export { func, log, taskA as task_a, taskB as task_b };
 
-let a: Employee = new Employee("Ivan", 17);
-let b: Employee = new Employee("Vlad", 18);
-let c: Employee = new Employee("Mikhail", 17, 20000);
+const a: Employee = new Employee('Ivan', 17);
+const b: Employee = new Employee('Vlad', 18);
+const c: Employee = new Employee('Mikhail', 17, 20000);
 
-console.log(a)
-console.log(b)
-console.log(c)
+console.log(a);
+console.log(b);
+console.log(c);
 b.age = 10;
 console.log(b);
 
 try {
-    c.age = -10;
-} catch(e) {
-    console.log(e.message)
+  c.age = -10;
+} catch (e) {
+  console.log(e.message);
 }
 
-console.log(task_a(1.2, 5.2, 0.8));
-console.log(task_b([1.9, 2.15, 2.34, 2.73, 3.16]));
+console.log(taskA(1.2, 5.2, 0.8));
+console.log(taskB([1.9, 2.15, 2.34, 2.73, 3.16]));
