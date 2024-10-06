b16cf6f02edfce9789e18bde3b2a1b29b1ef195e
smirnovg2005 <143895277+RouKou76@users.noreply.github.com>
2024-10-06 13:35:09 +0300

    Update index.ts

--- rpgsaga/saga/src/index.ts
+++ rpgsaga/saga/src/index.ts
@@ -1,11 +1,11 @@
-import { getTaskA, getTaskB } from "./CalcOfFunc";
+import { getTaskA, getTaskB } from './CalcOfFunc';
 
-const xN = 0.26,
-    xK = 0.66,
-    xD = 0.08,
-    numbersX = [0.1, 0.35, 0.4, 0.55, 0.6];
+const xN = 0.26;
+const xK = 0.66;
+const xD = 0.08;
+const numbersX = [0.1, 0.35, 0.4, 0.55, 0.6];
 
 console.log(getTaskA(xN, xK, xD));
 console.log(getTaskB(numbersX));
 
-export {getTaskA, getTaskB};
+export { getTaskA, getTaskB };
