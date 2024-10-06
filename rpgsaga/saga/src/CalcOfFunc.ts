6d4b618aecfc37cb2b4e8ba21114df361dd74994
smirnovg2005 <143895277+RouKou76@users.noreply.github.com>
2024-10-06 13:30:14 +0300

    Update CalcOfFunc.ts

--- rpgsaga/saga/src/CalcOfFunc.ts
+++ rpgsaga/saga/src/CalcOfFunc.ts
@@ -1,22 +1,22 @@
 export function calculate(x: number) {
-    const arcSin2 = (Math.asin(x)) ** 2;
-    const arcCos4 = (Math.acos(x)) ** 4;
-    const y = (arcSin2 + arcCos4) ** 3
-    return y;
+  const arcSin2 = Math.asin(x) ** 2;
+  const arcCos4 = Math.acos(x) ** 4;
+  const y = (arcSin2 + arcCos4) ** 3;
+  return y;
 }
 
 export function getTaskA(xN: number, xK: number, xD: number) {
-    const ansverA: number[] = [];
-    for (let i = xN; i <= xK; i += xD) {
-        ansverA.unshift(calculate(i));
-    }
-    return ansverA;
+  const ansverA: number[] = [];
+  for (let i = xN; i <= xK; i += xD) {
+    ansverA.unshift(calculate(i));
+  }
+  return ansverA;
 }
 
 export function getTaskB(numbersX: number[]) {
-    const ansverB: number[] = [];
-    for (let X of numbersX) {
-        ansverB.unshift(calculate(X));
-    }
-    return ansverB;
+  const ansverB: number[] = [];
+  for (let X of numbersX) {
+    ansverB.unshift(calculate(X));
+  }
+  return ansverB;
 }
