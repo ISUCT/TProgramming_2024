8043be812fe4a9df6c65713932756a85d438cb6d
Gleb <smirnovg2005@mail.ru>
2024-10-06 09:43:14 +0300

    CalcOfFunc and Tests

---
+++ rpgsaga/saga/src/CalcOfFunc.ts
@@ -0,0 +1,22 @@
+export function calculate(x: number) {
+    let arcSin2 = (Math.asin(x))**2;
+    let arcCos4 = (Math.acos(x))**4;
+    let y = (arcSin2 + arcCos4)**3
+    return y;
+}
+
+export function getTaskA(xN: number, xK: number, xD: number) {
+    const ansverA: number[] = [];
+    for (let i = xN; i <= xK; i += xD) {
+        ansverA.unshift(calculate(i));
+    }
+    return ansverA;
+}
+
+export function getTaskB(numbersX: number[]) {
+    const ansverB: number[] = [];
+    for (let X of numbersX) {
+        ansverB.unshift(calculate(X));
+    }
+    return ansverB;
+}
--- rpgsaga/saga/src/index.ts
+++ rpgsaga/saga/src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello world');
+import { getTaskA, getTaskB } from "./CalcOfFunc";
+
+const xN = 0.26,
+    xK = 0.66,
+    xD = 0.08,
+    numbersX = [0.1, 0.35, 0.4, 0.55, 0.6];
+
+console.log(getTaskA(xN, xK, xD));
+console.log(getTaskB(numbersX));
+
+export {getTaskA, getTaskB};
