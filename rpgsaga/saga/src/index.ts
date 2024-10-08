7ca364fda28fe5e05f8ffb1a3a8510be6bd4968a
kokter <koktera@gmail.com>
2024-10-08 11:00:47 +0300

    Lint fixed files

--- rpgsaga/saga/src/index.ts
+++ rpgsaga/saga/src/index.ts
@@ -1,7 +1,5 @@
-import { log } from "console";
-
 function form(x: number, a: number = 2.25) {
-    return ((a ** (x ** 2 - 1)) - Math.log((x ** 2) - 1) + Math.cbrt((x ** 2) - 1))
+    return a ** (x ** 2 - 1) - Math.log(x ** 2 - 1) + Math.cbrt(x ** 2 - 1);
 }
 
 function taskA(xStart: number = 1.2, xEnd: number = 2.7, step: number = 0.3, a: number = 2.25) {
@@ -9,7 +7,7 @@ function taskA(xStart: number = 1.2, xEnd: number = 2.7, step: number = 0.3, a: 
     const x: number[] = [];
     for (let i: number = xStart; i <= xEnd; i += step) {
         y.push(form(i, a));
-        x.push(i)
+        x.push(i);
     }
     return [x, y];
 }
@@ -17,7 +15,7 @@ function taskA(xStart: number = 1.2, xEnd: number = 2.7, step: number = 0.3, a: 
 function taskB(a: number = 2.25, xArr: number[] = [1.31, 1.39, 1.44, 1.56, 1.92]) {
     const y: number[] = [];
     const x: number[] = [];
-    for (let i of xArr) {
+    for (const i of xArr) {
         y.push(form(i, a));
         x.push(i);
     }
@@ -26,13 +24,13 @@ function taskB(a: number = 2.25, xArr: number[] = [1.31, 1.39, 1.44, 1.56, 1.92]
 
 console.log('Задача А');
 let [x, y] = taskA();
-for (let i of y) {
+for (const i of y) {
     console.log(`При x = ${x[y.indexOf(i)].toFixed(1)}, y = ${i.toFixed(2)}`);
 }
 console.log('Задача B');
 [x, y] = taskB();
-for (let i of y) {
+for (const i of y) {
     console.log(`При x = ${x[y.indexOf(i)].toFixed(2)}, y = ${i.toFixed(2)}`);
 }
 
-export { taskA, taskB, form }
+export { taskA, taskB, form };
