50718e03e0c6d3bfc66c06fd0cadd30d93818968
tenawan2gecko <artem.tchalunin@yandex.ru>
2024-10-08 10:14:40 +0300

    resolve problem

--- rpgsaga/saga/tests/example.spec.ts
+++ rpgsaga/saga/tests/example.spec.ts
@@ -47,3 +47,10 @@ describe('TaskB', () => {
   });
 });
 
+describe('Example', () => {
+  it('should return 5 as result of 2 and 3 sum', () => {
+    const a = 3;
+    const b = 2;
+    expect(a + b).toEqual(5);
+  });
+});
