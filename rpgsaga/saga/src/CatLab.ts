54254e89fe182af2541e0b6dabd684a75d9bc678
tenawan2gecko <artem.tchalunin@yandex.ru>
2024-10-08 11:10:21 +0300

    fix cat lab

--- rpgsaga/saga/src/CatLab.ts
+++ rpgsaga/saga/src/CatLab.ts
@@ -1,8 +1,8 @@
 import { Cat } from './LabWithClass/Cat';
 
-let cat = new Cat('Skaya', 10, 'British', 'Male');
+const cat = new Cat('Skaya', 10, 'British', 'Male');
 console.log(cat);
-let cat2 = new Cat('Melmon', 18, 'Tiger', 'Female');
+const cat2 = new Cat('Melmon', 18, 'Tiger', 'Female');
 console.log(cat2.age);
 cat2.age = 20;
 console.log(cat2.age);
