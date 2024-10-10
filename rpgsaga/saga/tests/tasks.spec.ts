bc55f7b91dbcb44db981f41fa5b0fe5795dcd6c0
kokter <koktera@gmail.com>
2024-10-10 18:36:38 +0300

    fixed tests

--- rpgsaga/saga/tests/tasks.spec.ts
+++ rpgsaga/saga/tests/tasks.spec.ts
@@ -1,12 +1,12 @@
-import { taskA, taskB} from "../src";
+import { taskA, taskB } from '../src/labs/formula';
 
 describe('Test of taskA and taskB', () => {
-    it('Function "taskA" should return answer with correct length', () => {
-        const [x, y] = taskA();
-        expect(y).toHaveLength(6);
-    });
-    it('Function "taskB" should return answer with correct length', () => {
-        const [x, y] = taskB();
-        expect(y).toHaveLength(5);
-    });
+  it('Function "taskA" should return answer with correct length', () => {
+    const [, y] = taskA();
+    expect(y).toHaveLength(6);
+  });
+  it('Function "taskB" should return answer with correct length', () => {
+    const [, y] = taskB();
+    expect(y).toHaveLength(5);
+  });
 });
