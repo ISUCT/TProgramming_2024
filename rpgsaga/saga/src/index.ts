afe7093ddacba06cb53bf741f8fd8fcb3b8a4504
UMBRELLA909 <mix.zhuckow2018@yandex.ru>
2024-10-12 22:29:40 +0300

    some silly fix + test for lab 1

--- rpgsaga/saga/src/index.ts
+++ rpgsaga/saga/src/index.ts
@@ -2,13 +2,13 @@ import { TaskA, TaskB } from './lab1';
 
 function main() {
     // TaskA
-    console.log("AnswerOnTask1:");
+    console.log("AnswerOnTaskA:");
     const resultSliceTaskA = TaskA(1.1, 0.09, 1.2, 2.2, 0.2);
     console.log(resultSliceTaskA);
 
     // TaskB
     const mySliceForTaskB = [1.21, 1.76, 2.53, 3.48, 4.52];
-    console.log("AnswerOnTask2:");
+    console.log("AnswerOnTaskB:");
     const resultSlice = TaskB(mySliceForTaskB, 1.1, 0.09);
     console.log(resultSlice);
 }
