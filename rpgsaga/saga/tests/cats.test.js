18ef4840bd0dd0eb55c6cd490d398781f09e0c74
MarkVRKS <143934528+MarkVRKS@users.noreply.github.com>
2024-10-17 21:58:10 +0300

    Update cats.test.js

--- rpgsaga/saga/tests/cats.test.js
+++ rpgsaga/saga/tests/cats.test.js
@@ -2,46 +2,46 @@ import {Cat} from '../src';
 
 test('Проверяем конструктор, инициализируем его свойства', () => {
     const cat = new Cat('Marsik', 2, 'The Lop-eared Scotsman');
-    expect(cat.getname).toBe('Marsik');
-    expect(cat.getage).toBe(2);
-    expect(cat.getbreed).toBe('The Lop-eared Scotsman');
+    expect(cat.getName).toBe('Marsik');
+    expect(cat.getAge).toBe(2);
+    expect(cat.getBreed).toBe('The Lop-eared Scotsman');
 });
 
 test('Получаем и устанавливаем имя кошки', () => {
     const cat = new Cat('Marsik', 2, 'The Lop-eared Scotsman');
-    cat.setname = 'Barsik';
-    expect(cat.getname).toBe('Barsik');
+    cat.setName = 'Barsik';
+    expect(cat.getName).toBe('Barsik');
 });
 
 test('Получаем и устанавливаем возраст кошки', () => {
     const cat = new Cat('Marsik', 2, 'The Lop-eared Scotsman');
-    cat.setage = 5;
-    expect(cat.getage).toBe(5);
+    cat.setAge = 5;
+    expect(cat.getAge).toBe(5);
 });
 
 test('Придумаем недопустимое значение возраста', () => {
     console.error = jest.fn();
     const cat = new Cat('Marsik', 2, 'The Lop-eared Scotsman');
-    cat.setage = 250; 
+    cat.setAge = 250; 
     expect(console.error).toHaveBeenCalledWith("Cats don't live that long!");
-    expect(cat.getage).toBe(2);
+    expect(cat.getAge).toBe(2);
 });
 
 test('Получаем и устанавливаем породу кошки', () => {
     const cat = new Cat('Marsik', 2, 'The Lop-eared Scotsman');
-    cat.setbreed = 'Siamese';
-    expect(cat.getbreed).toBe('Siamese');
+    cat.setBreed = 'Siamese';
+    expect(cat.getBreed).toBe('Siamese');
 });
 
 test('Получаем полные сведения о кошке', () => {
     const cat = new Cat('Marsik', 2, 'The Lop-eared Scotsman');
-    expect(cat.getinfo).toBe("Cat's name: Marsik, cat's age: 2, cat's breed: The Lop-eared Scotsman");
+    expect(cat.getInfo).toBe("Cat's name: Marsik, cat's age: 2, cat's breed: The Lop-eared Scotsman");
 });
 
 test('Меняем имя у кошки', () => {
     const cat = new Cat('Marsik', 2, 'The Lop-eared Scotsman');
     cat.changecatname('Barsik');
-    expect(cat.getname).toBe('Barsik');
+    expect(cat.getName).toBe('Barsik');
 });
 
 test('Узнаем откуда родом кошки', () => {
@@ -49,7 +49,8 @@ test('Узнаем откуда родом кошки', () => {
     const cat2 = new Cat('Kompot', 1, 'The mutt');
     const cat3 = new Cat('Simba', 6, 'Siamese');
     
-    expect(cat1.wherefrom()).toBe('Russia');
-    expect(cat2.wherefrom()).toBe('Belarus');
-    expect(cat3.wherefrom()).toBe("Ooops... I don't know(");
+    expect(cat1.whereFrom()).toBe('Russia');
+    expect(cat2.whereFrom()).toBe('Belarus');
+    expect(cat3.whereFrom()).toBe("Ooops... I don't know(");
 });
+
