37db95e41945cefbb367315cd8753121e84d054b
Gleb <smirnovg2005@mail.ru>
2024-10-17 12:59:25 +0300

    Changed Calc of func

--- rpgsaga/saga/src/CalcOfFunc.ts
+++ rpgsaga/saga/src/CalcOfFunc.ts
@@ -1,8 +1,5 @@
 export function calculate(x: number) {
-  const arcSin2 = Math.asin(x) ** 2;
-  const arcCos4 = Math.acos(x) ** 4;
-  const y = (arcSin2 + arcCos4) ** 3;
-  return y;
+  return (Math.acos(x) ** 4 + Math.asin(x) ** 2) ** 3;
 }
 
 export function getTaskA(xN: number, xK: number, xD: number) {
