67c6dd1232463b592c14ceebe027530dddb40deb
Gleb <smirnovg2005@mail.ru>
2024-10-17 09:47:51 +0300

    Revert "Merge branch 'master' of https://github.com/RouKou77/TProgramming_2024"
    This reverts commit 40fed7b775e00c393c736691b60a41bad1e9714b, reversing
    changes made to b9092aafaead46def30a0dd4f6c79af170ada1c4.

--- rpgsaga/saga/src/index.ts
+++ rpgsaga/saga/src/index.ts
@@ -1,10 +1,11 @@
 import { getTaskA, getTaskB } from './CalcOfFunc';
 
-// Константы для Лабы 1(CalcOfFunc)
 const xN = 0.26;
 const xK = 0.66;
 const xD = 0.08;
 const numbersX = [0.1, 0.35, 0.4, 0.55, 0.6];
-// Вычисления и вывод для Лабы 1 (CalcOfFunc)
+
 console.log(getTaskA(xN, xK, xD));
 console.log(getTaskB(numbersX));
+
+export { getTaskA, getTaskB };
--- rpgsaga/saga/tests/СharacterClass.test.ts
+++
@@ -1,9 +0,0 @@
-import { Character } from "../src/Сharacter";
-
-describe("Class Character", () =>{
-    it("create Character Warrior", () => {
-        const war = new Character("Name", "Warrior");
-
-        expect(war).toEqual({"class": "Warrior", "level": 1, "name": "Name"});
-    });
-});
