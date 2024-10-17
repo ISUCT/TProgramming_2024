7d5f8ebb46b905bd31de3f840363e3fd30cc2b04
Gleb <smirnovg2005@mail.ru>
2024-10-17 16:40:43 +0300

    Changed CalcOfFunc test

--- rpgsaga/saga/tests/CalcOfFunc.test.ts
+++ rpgsaga/saga/tests/CalcOfFunc.test.ts
@@ -3,12 +3,8 @@ import { getTaskA, getTaskB } from '../src/CalcOfFunc';
 describe('TaskA', () => {
   it('correct answer A', () => {
     const expectedResult = [
-      1.1298385487203866,
-      1.7472761588502188,
-      3.220397731030495,
-      6.447227535266805,
-      13.21117766406966,
-      26.84410487237168
+      1.1298385487203866, 1.7472761588502188, 3.220397731030495, 6.447227535266805, 13.21117766406966,
+      26.84410487237168,
     ];
     const result = getTaskA(0.26, 0.66, 0.08);
     expect(result).toEqual(expectedResult);
@@ -26,11 +22,7 @@ describe('TaskA', () => {
 describe('TaskB', () => {
   it('correct answer B', () => {
     const expectedResult = [
-      1.5347282613199904,
-      2.16507891445927,
-      7.709481911813682,
-      12.078220437571701,
-      102.99803805648071
+      1.5347282613199904, 2.16507891445927, 7.709481911813682, 12.078220437571701, 102.99803805648071,
     ];
     const result = getTaskB([0.1, 0.35, 0.4, 0.55, 0.6]);
     expect(result).toEqual(expectedResult);
