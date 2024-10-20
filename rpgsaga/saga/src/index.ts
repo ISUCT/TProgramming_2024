af0601314e4f620161d46d83098ef2932721d09a
Vladislav <whatislovekuvshinov@gmail.com>
2024-10-20 12:47:16 +0300

    tasks+character

--- rpgsaga/saga/src/index.ts
+++ rpgsaga/saga/src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello world');
+import { taskone, tasktwo } from './tasks';
+import { Character } from './character';
+
+console.log(taskone(0.26, 0.66, 0.08));
+console.log(tasktwo(0.1, 0.35, 0.4, 0.55, 0.6));
+
+const chel = new Character("Владик Пармезан", 7, 8, 7, 8);
+chel.printOverallInfo();
+chel.event();
