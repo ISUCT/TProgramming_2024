fe3a4212ec3bd612160f1a3f530fe8896a5a8d63
J0es1ick <mrjoeslick@gmail.com>
2024-10-21 18:37:20 +0300

    rpg saga

--- rpgsaga/saga/src/index.ts
+++ rpgsaga/saga/src/index.ts
@@ -1,51 +1,17 @@
-function result(a: number, x: number): number {
-  return Math.pow(a, Math.pow(x, 2) - 1) - Math.log10(Math.pow(x, 2) - 1) + Math.cbrt(Math.pow(x, 2) - 1);
+class Gamer {
+  protected _health: number;
+  protected _strength: number;
+  protected _name: string;
+
+  constructor(gamerHealth, gamerStrength, gamerName) {
+    this._health = gamerHealth;
+    this._strength = gamerStrength;
+    this._name = gamerName;
+  }
 }
 
-function taskA(a: number, xBegin: number, xEnd: number, xDelta: number): number[] {
-  const y: number[] = [];
-  if (xDelta === 0) {
-    return [];
-  }
-  if (xDelta > 0) {
-    for (let x = xBegin; x <= xEnd; x += xDelta) {
-      y.push(result(a, x));
-    }
-  } else {
-    for (let x = xBegin; x >= xEnd; x += xDelta) {
-      if (Math.pow(x, 2) - 1 <= 0) {
-        continue;
-      }
-      y.push(result(a, x));
-    }
-  }
-  return y;
-}
+class Knight extends Gamer {}
 
-function taskB(a: number, values: number[]): number[] {
-  if (values.length === 0) {
-    return [];
-  }
-  const y: number[] = [];
-  for (const x of values) {
-    y.push(result(a, x));
-  }
-  return y;
-}
+class Vizard extends Gamer {}
 
-function output(nameOfTask: string, results: number[]): string {
-  let resMsg = `Solutions to task ${nameOfTask}:\n`;
-
-  results.forEach(resultNum => {
-    resMsg += `${resultNum},\n`;
-  });
-  return resMsg;
-}
-
-const taskAResult = taskA(1.6, 2, 10, 0);
-console.log(output('A', taskAResult));
-
-const taskBResult = taskB(1.6, []);
-console.log(output('B', taskBResult));
-
-export { taskA, taskB };
+class Archer extends Gamer {}
--- rpgsaga/saga/tests/example.spec.ts
+++ rpgsaga/saga/tests/example.spec.ts
@@ -1,4 +1,4 @@
-import { taskA, taskB } from '../src';
+import { taskA, taskB } from '../src/function';
 
 describe('Tests taskA', () => {
   it('should return 6 values', () => {
