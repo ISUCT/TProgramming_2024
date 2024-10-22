ef2d689a645361bd5257004903adb7cb600d49b8
DariaSh <dameshiks@gmail.com>
2024-10-22 10:08:58 +0300

    .

--- rpgsaga/saga/src/function.ts
+++ rpgsaga/saga/src/function.ts
@@ -4,66 +4,64 @@ const Step = 0.4;
 const ArrayOfX = [1.9, 2.15, 2.34, 2.74, 3.16];
 
 function getBaseLog(base: number, numb: number): number {
-    return Number((Math.log(numb)/Math.log(base)).toFixed(2));
+  return Number((Math.log(numb) / Math.log(base)).toFixed(2));
 }
 
-
-function MainFunction(a:number, b:number, x:number) : number {
-
-    let top = a*Math.pow(x, 1.0/3) - b*getBaseLog(5, x);
-    let bottom = Math.pow(Math.log10(x-1), 3.0);
-    let result = (top/bottom).toFixed(2);
-    return Number(result);
-    
+export function mainFunction(a: number, b: number, x: number): number {
+  const top = a * Math.pow(x, 1.0 / 3) - b * getBaseLog(5, x);
+  const bottom = Math.pow(Math.log10(x - 1), 3.0);
+  const result = (top / bottom).toFixed(2);
+  return Number(result);
 }
 
-function taskA (startingX:number, endingX:number, step:number) : [number[], number[]]{
-    
-    const A = 4.1;
-    const B = 2.7;
+export function taskA(startingX: number, endingX: number, step: number): [number[], number[]] {
+  if (step === 0 && startingX !== endingX) {
+    return [[], []];
+  }
 
-    let x: number[] = [];
-    let y: number[] = [];
+  const A = 4.1;
+  const B = 2.7;
 
-    for (let i = startingX; i <= endingX; i += step){
-        x.push(Number(i.toFixed(2)));
-        y.push(MainFunction(A, B, i));
+  const x: number[] = [];
+  const y: number[] = [];
 
-    }
+  for (let i = startingX; i <= endingX; i += step) {
+    x.push(Number(i.toFixed(2)));
+    y.push(mainFunction(A, B, i));
+  }
 
-    return [x, y];
-
+  return [x, y];
 }
 
-function taskB (arrayOfX: number[]): [number[], number[]] {
-    
-    const A = 4.1;
-    const B = 2.7;
+export function taskB(arrayOfX: number[]): [number[], number[]] {
+  const A = 4.1;
+  const B = 2.7;
 
-    let x: number[] = [];
-    let y: number[] = [];
+  const x: number[] = [];
+  const y: number[] = [];
 
+  for (let i = 0; i < arrayOfX.length; i++) {
+    x.push(arrayOfX[i]);
+    y.push(mainFunction(A, B, arrayOfX[i]));
+  }
 
-    for (let i = 0; i < arrayOfX.length; i ++){
-        x.push(arrayOfX[i]);
-        y.push(MainFunction(A, B, arrayOfX[i]));
-    }
-
-    return [x,y];
-
+  return [x, y];
 }
 
-console.log("Задача A");
-let [x, y] = taskA(StartingX, EndingX, Step);
-for (var i of y){
+function testingA() {
+  const [x, y] = taskA(StartingX, EndingX, Step);
+  for (const i of y) {
     console.log(`При x = ${x[y.indexOf(i)]} y = ${i}`);
+  }
 }
 
-
-console.log("Задача B");
-[x,y] = taskB(ArrayOfX);
-for (var i of y) {
+function testingB() {
+  console.log('Задача B');
+  const [x, y] = taskB(ArrayOfX);
+  for (const i of y) {
     console.log(`При x = ${x[y.indexOf(i)]} y = ${i}`);
+  }
 }
 
-export {MainFunction,taskA,taskB};
+testingA();
+testingB();
