580298718b13423c98e4fc1584d2d57128d2d15a
tenawan2gecko <artem.tchalunin@yandex.ru>
2024-10-22 10:16:38 +0300

    Add test

--- rpgsaga/saga/tests/example.spec.ts
+++ rpgsaga/saga/tests/example.spec.ts
@@ -1,4 +1,5 @@
 import { mathFunction, taskA, taskB } from '../src/MathCalc';
+import { Cat } from '../src/LabWithClass/Cat';
 
 describe('MathFunction', () => {
   test('should return correct result for given inputs', () => {
@@ -54,3 +55,51 @@ describe('Example', () => {
     expect(a + b).toEqual(5);
   });
 });
+
+describe('Cat class', () => {
+  let cat: Cat;
+
+  beforeEach(() => {
+    cat = new Cat('Whiskers', 2, 'female', 'women');
+  });
+
+  test('should create a cat instance with valid properties', () => {
+    expect(cat.name).toBe('Whiskers');
+    expect(cat.age).toBe(2);
+    expect(cat.gender).toBe('female');
+    expect(cat.male).toBe('women');
+  });
+
+  test('should set name correctly', () => {
+    cat.name = 'Garfield';
+    expect(cat.name).toBe('Garfield');
+  });
+
+  test('should set age correctly', () => {
+    cat.age = 3;
+    expect(cat.age).toBe(3);
+  });
+
+  test('should throw an error if age is negative', () => {
+    expect(() => {
+      cat.age = -1;
+    }).toThrow('Invalid age');
+  });
+
+  test('should set gender correctly', () => {
+    cat.gender = 'male';
+    expect(cat.gender).toBe('male');
+  });
+
+  test('should set male correctly when valid', () => {
+    cat.male = 'man';
+    expect(cat.male).toBe('man');
+  });
+
+  test('should throw an error when setting an invalid male value', () => {
+    expect(() => {
+      cat.male = 'invalid';
+    }).toThrow('Invalid male');
+  });
+});
+
