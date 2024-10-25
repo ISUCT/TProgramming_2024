0b46bcafd1aca5d8e21063c36bbb73232d0666c4
UMBRELLA909 <mix.zhuckow2018@yandex.ru>
2024-10-25 20:17:29 +0300

    Try_to_fix_lint_2

--- rpgsaga/saga/src/index.ts
+++ rpgsaga/saga/src/index.ts
@@ -1,15 +1,15 @@
-import { TaskA, TaskB } from './lab1';
+import { taskA, taskB } from './lab1';
 
 function main() {
-    // TaskA
-    console.log("AnswerOnTaskA:");
-    const resultSliceTaskA = TaskA(1.1, 0.09, 1.2, 2.2, 0.2);
-    console.log(resultSliceTaskA);
+  // TaskA
+  console.log('AnswerOnTaskA:');
+  const resultSliceTaskA = taskA(1.1, 0.09, 1.2, 2.2, 0.2);
+  console.log(resultSliceTaskA);
 
-    // TaskB
-    const mySliceForTaskB = [1.21, 1.76, 2.53, 3.48, 4.52];
-    console.log("AnswerOnTaskB:");
-    const resultSlice = TaskB(mySliceForTaskB, 1.1, 0.09);
-    console.log(resultSlice);
+  // TaskB
+  const mySliceForTaskB = [1.21, 1.76, 2.53, 3.48, 4.52];
+  console.log('AnswerOnTaskB:');
+  const resultSlice = taskB(mySliceForTaskB, 1.1, 0.09);
+  console.log(resultSlice);
 }
 main();
--- rpgsaga/saga/tests/My_tests.spec.ts
+++ rpgsaga/saga/tests/My_tests.spec.ts
@@ -1,8 +1,8 @@
-import { TaskA, TaskB } from '../src/lab1';
+import { taskA, taskB } from '../src/lab1';
 
-describe('TaskA', () => {
+describe('taskA', () => {
     it('should calculate the correct values for TaskA', () => {
-      const testresultTaskA = TaskA(1.1, 0.09, 1.2, 2.2, 0.2);
+      const testresultTaskA = taskA(1.1, 0.09, 1.2, 2.2, 0.2);
       const trueresultTaskA = [
         -0.5527140818130227, 0.5357584954022472, 0.7876803233945551, 0.8841676026091737, 0.9309997512495364,
         0.9572340900142425,
@@ -11,10 +11,10 @@ describe('TaskA', () => {
     });
   });
   
-  describe('TaskB', () => {
+  describe('taskB', () => {
     it('should calculate the correct values for TaskB', () => {
       const mySliceForTaskB = [1.21, 1.76, 2.53, 3.48, 4.52];
-      const testresultTaskB = TaskB(mySliceForTaskB, 1.1, 0.09);
+      const testresultTaskB = taskB(mySliceForTaskB, 1.1, 0.09);
       const trueresultTaskB = [
         -0.4325793499470868, 0.8704142934030041, 0.9808944539200825, 1.0060147033160582, 1.0152114712907188,
       ];
