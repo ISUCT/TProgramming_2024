dc83f66de75f59756db9c9bf1081f5f243c482a8
kokter <koktera@gmail.com>
2024-10-30 21:25:19 +0300

    Done SuperClass Lab

--- rpgsaga/saga/src/labs/picture.ts
+++ rpgsaga/saga/src/labs/picture.ts
@@ -1,20 +1,27 @@
-import { title } from "process";
-import { workOfArt } from "./superClass";
+import { WorkOfArt } from './workOfArt';
 
-export class Picture extends workOfArt {
+export class Picture extends WorkOfArt {
   private _style: string;
+  private _url: string;
   constructor(
     name: string,
     creator: string,
     year: number,
     country: string = '<unknown country>',
-    style: string
+    style: string,
+    pictureURL: string,
   ) {
-    super(name,year,creator,country);
+    super(name, year, creator, country);
     this._style = style;
+    this._url = pictureURL;
   }
   public get style(): string {
     return this._style;
   }
-  
+  public get url(): string {
+    return this._url;
+  }
+  public display() {
+    console.log(`URL to "${this.name}" is '${this.url}'`);
+  }
 }
