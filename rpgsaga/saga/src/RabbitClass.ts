3ed58241f58084bf120cedb4fa6d79e74815da81
HaQmUser <pk@dpms.ru>
2024-11-05 11:24:50 +0300

    //

---
+++ rpgsaga/saga/src/RabbitClass.ts
@@ -0,0 +1,46 @@
+export class Rabbit {
+    private _name: string;
+    private _age: number;
+    private _color: string;
+    private _gender: string;
+    constructor(name: string, age: number, gender: string, color: string) {
+      this._name = name;
+      this._age = age;
+      this._color = color;
+      this._gender = gender;
+    }
+    
+    public set age(age: number) {
+        if (age < 0) {
+            throw new Error('Invalid age');
+        }
+        this._age = age;
+    }
+    public set name(name: string) {
+        this._name = name;
+      }
+    public set color(color: string) {
+        this.color = color;
+      }
+    public set gender(gender: string) {
+        if (gender.toLowerCase() == 'male'){
+            this._gender = 'Male';
+        } else if (gender.toLowerCase() == 'female'){
+            this._gender = 'Female'
+        } else {
+            throw new Error('Invalid gender')
+        }
+      }
+      public get age() {
+        return this._age;
+      }
+      public get name() {
+        return this._name;
+      }
+      public get gender() {
+        return this._gender;
+      }
+      public get color() {
+        return this._color;
+    }
+}
--- rpgsaga/saga/tests/mathematics.spec.ts
+++ rpgsaga/saga/tests/mathematics.spec.ts
@@ -3,17 +3,27 @@ import { formula, taskA, taskB } from '../src/mathematics';
 
 describe('formula', () => {
     test('should return right result', () => {
-      const result = formula(0.25);
-      expect(result).toBeCloseTo(0.168);
+      expect(formula(0.25)).toBeCloseTo(0.168);
     });
   
     test('This Arccos doesnt excist', () => {
-      const result = formula(2,2);
-      expect(result).toBeNaN();
+      expect(formula(2.2)).toBeNaN();
     });
   
     test('should handle division by zero in denominator', () => {
-      const result = formula(1, 2, 3);
+      const result = formula(1);
       expect(result).toBeDefined(); 
     });
   });
+
+describe('TaskA',()=> {
+  test('should return right size',()=>{
+    expect(taskA(0.7,2.2,0.3)).toHaveLength(5);
+  })
+})
+
+describe('TaskB',()=> {
+    test('should return right size',()=>{
+      expect(taskB([0.25, 0.36, 0.56, 0.94, 1.28])).toHaveLength(5);
+    })
+  })
