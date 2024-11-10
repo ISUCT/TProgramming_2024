073fb232c793d3d2655d42ec67b2070e4088f472
rusafob <147656000+rusafob@users.noreply.github.com>
2024-11-10 23:13:01 +0300

    Update index.ts

--- rpgsaga/saga/src/index.ts
+++ rpgsaga/saga/src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello world');
+import {task19A, task19B} from './math';
+
+const a = 2.0;
+const x1 = 1.16;
+const x2 = 1.32;
+const x3 = 1.47;
+const x4 = 1.65;
+const x5 = 1.93;
+let xn = 1.2;
+let xk = 4.2;
+let dx = 0.6;
+const xs: number[] = [x1, x2, x3, x4, x5];
