c05abd1edf29316ba4a1a21182fdc5323f88817b
RenatZhappuev <RenatZhappuev@yandex.ru>
2024-11-13 08:47:44 +0300

    lab3 fix

--- rpgsaga/saga/tests/test.spec.ts
+++ rpgsaga/saga/tests/test.spec.ts
@@ -1,36 +1,34 @@
-import { Bunny } from '../src/bunny'
+import { Bunny } from '../src/bunny';
 
-describe ('Constructor test', () => {
-    it('should create a bunny', () => {
-        let bunny = new Bunny('Test', 2 , 5);
-        expect(bunny.name).toStrictEqual('Test')
-        expect(bunny.age).toStrictEqual(2)
-        expect(bunny.weight).toStrictEqual(5)
-    })
-})
+describe('Constructor test', () => {
+  it('should create a bunny', () => {
+    const bunny = new Bunny('Test', 2, 5);
+    expect(bunny.name).toStrictEqual('Test');
+    expect(bunny.age).toStrictEqual(2);
+    expect(bunny.weight).toStrictEqual(5);
+  });
+});
 
-describe ('SetAge func test', () => {
-    it('should set 5', () => {
-        let bunny = new Bunny('Test', 5 , 2);
-        expect(bunny.age).toStrictEqual(5);
-    })
-    it('should throw error', () => {
+describe('SetAge func test', () => {
+  it('should set 5', () => {
+    const bunny = new Bunny('Test', 5, 2);
+    expect(bunny.age).toStrictEqual(5);
+  });
+  it('should throw error', () => {
+    expect(() => {
+      new Bunny('Test', 200, 5);
+    }).toThrow('Wrong age');
+  });
+});
 
-        expect(() => {
-            let bunny = new Bunny('Test', 200 , 5);            
-        }).toThrowError('Wrong age')
-    })
-})
-
-describe ('SetWeight func test', () => {
-    it('should set 4', () => {
-        let bunny = new Bunny('Test', 5 , 4);
-        expect(bunny.weight).toStrictEqual(4);
-    })
-    it('should throw error', () => {
-
-        expect(() => {
-            let bunny = new Bunny('Test', 2 , 500);            
-        }).toThrowError('Wrong weight')
-    })
-})
+describe('SetWeight func test', () => {
+  it('should set 4', () => {
+    const bunny = new Bunny('Test', 5, 4);
+    expect(bunny.weight).toStrictEqual(4);
+  });
+  it('should throw error', () => {
+    expect(() => {
+      new Bunny('Test', 2, 500);
+    }).toThrow('Wrong weight');
+  });
+});
