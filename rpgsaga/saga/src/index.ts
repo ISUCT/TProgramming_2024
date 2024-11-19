4adde030bd70fbcaef8289d63edbe440bb6e47f4
makhotinna <makhotina2005@mail.ru>
2024-11-19 17:10:26 +0300

    math lab

--- rpgsaga/saga/src/index.ts
+++ rpgsaga/saga/src/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello world');
+export const calc = (a: number, b: number, x: number): number => {
+    const answer = ((a + Math.tan(b * x)) ^ 2) / ((b + 1 / Math.tan(a * x)) ^ 2);
+    return answer;
+  };
+
+  export const taskA = (a: number, b: number, x1: number, x2: number, deltax: number): number[] => {
+    const answersA: number[] = [];
+    let x = x1;
+    for (x; x < x2; x += deltax) {
+      answersA.push(calc(a, b, x));
+    }
+    return answersA;
+  };
+
+  export const taskB = (a: number, b: number, znX: number[]): number[] => {
+    const answersB: number[] = [];
+    for (let i = 0; i < 5; i++) {
+      answersB.push(calc(a, b, znX[i]));
+    }
+    return answersB;
+  };
+
+  console.log(taskA(0.1, 0.5, 0.15, 1.37, 0.25));
+  console.log(taskB(0.1, 0.5, [0.2, 0.3, 0.44, 0.6, 0.56]));
