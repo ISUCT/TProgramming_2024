2fa20323a773fa7aaeb10d9814f4d497f47279bb
GoidGoidGoida <megabebra@gmail.com>
2024-11-21 15:12:15 +0300

    Linter fixes 4.1

--- rpgsaga/saga/src/mathexpression/src/index.ts
+++ rpgsaga/saga/src/mathexpression/src/index.ts
@@ -1,7 +1,7 @@
 import { taskA, taskB } from "./mathFunction";
 
-let x: number[][] = taskA(1, 2, 3, 4, 0.5);
-let y: number[] = taskB([1, 2, 3], 2, 3);
+const x: number[][] = taskA(1, 2, 3, 4, 0.5);
+const y: number[] = taskB([1, 2, 3], 2, 3);
 
 console.log(x);
 console.log(y);
