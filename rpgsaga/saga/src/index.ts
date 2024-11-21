4316841157b046ca0e7064a563041ba4f24a4972
Ilya Paramonov <ilun37rus@gmail.com>
2024-11-21 13:00:29 +0300

    add output for lab2

--- rpgsaga/saga/src/index.ts
+++ rpgsaga/saga/src/index.ts
@@ -1,4 +1,5 @@
 import { task14A, task14B } from './lab1';
+import { Dish } from './lab2';
 
 const a = 7.2;
 const b = 4.2;
@@ -9,3 +10,9 @@ const xs: number[] = [2.4, 2.8, 3.9, 4.7, 3.16];
 
 console.log(task14A(a, b, xn, xk, dx));
 console.log(task14B(a, b, xs));
+
+const dish1 = new Dish("Pizza", 15.99, "Italian");
+const dish2 = new Dish("Apple pie", 8.99, "American");
+
+console.log(dish1.getinfo);
+console.log(dish2.getinfo);
