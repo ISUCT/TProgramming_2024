b40561473710dc311e3e5225575025d6c3285a9e
makhotinna <makhotina2005@mail.ru>
2024-11-21 14:20:34 +0300

    math lab + lint fix

--- rpgsaga/saga/tests/example.spec.ts
+++ rpgsaga/saga/tests/example.spec.ts
@@ -1,7 +1,23 @@
-describe('Example', () => {
-  it('should return 5 as result of 2 and 3 sum', () => {
-    const a = 3;
-    const b = 2;
-    expect(a + b).toEqual(5);
+import * as pointer from '../src/index';
+
+describe('calc', () => {
+  it('should not be NaN', () => {
+    expect(Number.isNaN(pointer.calc(1.2, 0.48, 0.7))).toStrictEqual(false);
+  });
+  it('should be NaN', () => {
+    expect(pointer.calc(1.2, 0.48, 15)).toStrictEqual(0);
+  });
+});
+
+describe('taskA', () => {
+  it('should be empty if x1 > x2', () => {
+    expect(pointer.taskA(1.2, 0.48, 2, 0.5, 0.3)).toStrictEqual([]);
+  });
+});
+
+describe('taskB', () => {
+  const expected = 5;
+  it('length of resulting array should equal 6', () => {
+    expect(pointer.taskB(1.2, 0.48, [0.25, 0.36, 0.56, 0.94, 1.28, 1.7]).length).toStrictEqual(expected);
   });
 });
