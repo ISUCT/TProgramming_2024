5a40508a45cee993f95c2f744a1e46cae8ebb70c
rusafob <147656000+rusafob@users.noreply.github.com>
2024-11-27 23:37:02 +0300

    Update index.ts

--- rpgsaga/saga/src/index.ts
+++ rpgsaga/saga/src/index.ts
@@ -1,4 +1,6 @@
 import {task19A, task19B} from './math';
+import { Mouse } from './src/labclass2';
+
 
 const a = 2.0;
 const x1 = 1.16;
@@ -10,3 +12,26 @@ let xn = 1.2;
 let xk = 4.2;
 let dx = 0.6;
 const xs: number[] = [x1, x2, x3, x4, x5];
+
+const cat1 = new Mouse('Roly', 5, 'Satin mice','Grey');
+console.log(cat1.getInfo);
+const cat2 = new Kitten('Mickey', 2, 'Yellow-throated mice','Yellow');
+console.log(Cat's name: ${cat3.name});
+console.log(cat3.changeCatName('Tyutya'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
