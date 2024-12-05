2fd4df5ea79bf24fecb5fe1bbcbe85fcf42bd203
Kaputmeoy <nastoyalove2005lol@gmail.com>
2024-12-05 13:00:47 +0300

    Lab 1

--- rpgsaga/saga/src/index.ts
+++ rpgsaga/saga/src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello world');
+import { taskA, taskB } from '../src/lab1';
+
+console.log('Терентьева Анастасия | 2/280 |  Вариант');
+
+console.log('Лабораторная работа с мат. функциями');
+console.log('Ответы для задачи A:');
+console.log(taskA(1.23, 7.23, 1.2));
+console.log('Ответы для задачи B:');
+console.log(taskB([1.88, 2.26, 3.84, 4.55, -6.21]));
---
+++ rpgsaga/saga/src/lab1.ts
@@ -0,0 +1,25 @@
+export function taskA(start: number, finish: number, delta: number): number[] {
+    const slice: number[] = [];
+    for (let x = start; x <= finish; x += delta) {
+        slice.push(solution(x));
+    }
+    return slice;
+}
+
+export function taskB(long: number[]): number[] {
+    const answer: number[] = [];
+    for (const val of long) {
+        answer.push(solution(val));
+    }
+    return answer;
+}
+
+export function solution(x: number): number {
+    const result = (Math.pow(Math.pow((x - 0.8), 2), drob(3.0)) + Math.pow(Math.abs(x + 0.4), drob(5.0))) /
+                    (Math.pow((Math.pow(x, 2) - Math.pow((0.8 + 0.4), 2)), drob(9.0)));
+    return result;
+}
+
+export function drob(y: number): number {
+    return 1 / y;
+}
