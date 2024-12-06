92d6c046f36681856f234c13ea68f62e8c347610
GoidGoidGoida <megabebra@gmail.com>
2024-12-06 19:39:51 +0300

    lint fixes 5

--- rpgsaga/saga/src/heroes/effects/burnEffect.ts
+++
@@ -1,9 +0,0 @@
-import { Effect } from "./effects";
-import { Hero } from "../heroes/hero";
-
-export class BurnEffect extends Effect {
-    apply(target: Hero) {
-        console.log(`${target.constructor.name} поджигается!`);
-        target.takeDamage(5);
-    }
-}
--- rpgsaga/saga/src/heroes/effects/effects.ts
+++
@@ -1,5 +0,0 @@
-import { Hero } from "../heroes/hero";
-
-export abstract class Effect {
-    abstract apply(target: Hero): void;
-}
